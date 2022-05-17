d72d3b05755d19ef44b148b6027eb04264ce0a40
Elena Carey <theelenacarey@gmail.com>
2022-05-16 21:45:01 -0500

    Refactored code to be easier to understand. Split task handler and task creation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,34 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //Creates tasks
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type").value;
     
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    
+    //Send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+    
+    
+};
+
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
    
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
-
-    
-    
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
